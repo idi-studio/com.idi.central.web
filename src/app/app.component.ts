8898d47cdc4196e45448ed2dc120e7bae24a802d
johnnywang <johnny_l_wang@manulife.com>
2017-07-24 13:14:44 +0800

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app-root.component.html'
+  templateUrl: './app.component.html'
 })
-export class AppRootComponent {
+export class AppComponent {
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 //Component
-import { AppRootComponent } from './components/app-root/app-root.component';
+import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
 import { LockScreenComponent } from './components/pages/lock-screen/lock-screen.component';
@@ -23,7 +23,7 @@ import { UserListComponent } from './components/users/user-list.component';
 
 @NgModule({
   declarations: [
-    AppRootComponent,
+    AppComponent,
     MainComponent, LoginComponent, ForgotPasswordComponent, LockScreenComponent,
     TopbarComponent, SidebarComponent, SidebarRightComponent, FootbarComponent, PageHeaderComponent,
     DashboardComponent, RoleListComponent, UserListComponent
@@ -31,6 +31,6 @@ import { UserListComponent } from './components/users/user-list.component';
   exports: [],//declarations 的子集，可用于其它模块的组件模板。
   imports: [BrowserModule, FormsModule, AppRoutingModule],//本模块声明的组件模板需要的类所在的其它模块
   providers: [],// 服务的创建者，并加入到全局服务列表中，可用于应用任何部分
-  bootstrap: [AppRootComponent]//指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性
+  bootstrap: [AppComponent]//指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性
 })
 export class AppModule { }
