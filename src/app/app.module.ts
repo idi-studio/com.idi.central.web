342d5fe61f731613505d868093e8696019b16dd7
johnnywang <johnny_l_wang@manulife.com>
2017-07-24 14:05:53 +0800

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,46 @@
 //Angular
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule, Type } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Module
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, CentralComponents } from './app.routing.module';
+import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
+// import { CovalentHighlightModule } from '@covalent/highlight';
+// import { CovalentMarkdownModule } from '@covalent/markdown';
+import { SharedModule } from './app.shared.module';
 
-//Component
+//Common
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/pages/login/login.component';
-import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
-import { LockScreenComponent } from './components/pages/lock-screen/lock-screen.component';
-import { MainComponent } from './components/pages/main/main.component';
-import { TopbarComponent } from './components/shared/topbar/topbar.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
-import { SidebarRightComponent } from './components/shared/sidebar-right/sidebar-right.component';
-import { FootbarComponent } from './components/shared/footbar/footbar.component';
-import { PageHeaderComponent } from './components/shared/page-header/page-header.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { RoleListComponent } from './components/roles/role-list.component';
-import { UserListComponent } from './components/users/user-list.component';
+import { RequestInterceptor } from './configs/interceptors/request.interceptor';
+import { API } from './configs/api.config';
+
+const httpInterceptorProviders: Type<any>[] = [
+  RequestInterceptor
+];
+
+export function getAPI(): string {
+  return API;
+}
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent, LoginComponent, ForgotPasswordComponent, LockScreenComponent,
-    TopbarComponent, SidebarComponent, SidebarRightComponent, FootbarComponent, PageHeaderComponent,
-    DashboardComponent, RoleListComponent, UserListComponent
+    AppComponent, CentralComponents
   ],
   exports: [],//declarations 的子集，可用于其它模块的组件模板。
-  imports: [BrowserModule, FormsModule, AppRoutingModule],//本模块声明的组件模板需要的类所在的其它模块
-  providers: [],// 服务的创建者，并加入到全局服务列表中，可用于应用任何部分
+  imports: [
+    AppRoutingModule, BrowserModule, BrowserAnimationsModule, SharedModule,
+    CovalentHttpModule.forRoot({
+      interceptors: [{
+        interceptor: RequestInterceptor, paths: ['**'],
+      }],
+      // CovalentHighlightModule, CovalentMarkdownModule,
+    }),
+  ],//本模块声明的组件模板需要的类所在的其它模块
+  providers: [
+    httpInterceptorProviders, Title,
+    // { provide: USERS_API, useFactory: getAPI, }
+  ],// 服务的创建者，并加入到全局服务列表中，可用于应用任何部分
   bootstrap: [AppComponent]//指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -9,6 +9,18 @@ import { MainComponent } from './components/pages/main/main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RoleListComponent } from './components/roles/role-list.component';
 import { UserListComponent } from './components/users/user-list.component';
+// import { LoginComponent } from './components/pages/login/login.component';
+// import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
+// import { LockScreenComponent } from './components/pages/lock-screen/lock-screen.component';
+// import { MainComponent } from './components/pages/main/main.component';
+import { TopbarComponent } from './components/shared/topbar/topbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { SidebarRightComponent } from './components/shared/sidebar-right/sidebar-right.component';
+import { FootbarComponent } from './components/shared/footbar/footbar.component';
+import { PageHeaderComponent } from './components/shared/page-header/page-header.component';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+// import { RoleListComponent } from './components/roles/role-list.component';
+// import { UserListComponent } from './components/users/user-list.component';
 
 const routes: Route[] = [
     { path: '', redirectTo: "/login", pathMatch: "full" },
@@ -31,3 +43,10 @@ const routes: Route[] = [
 export class AppRoutingModule {
 
 }
+
+export const CentralComponents: any[] = [
+    MainComponent, TopbarComponent, SidebarComponent, SidebarRightComponent, FootbarComponent, PageHeaderComponent,
+    DashboardComponent,
+    LoginComponent, ForgotPasswordComponent, LockScreenComponent,
+    RoleListComponent, UserListComponent
+];
