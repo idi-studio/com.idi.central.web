bc01960d6bd72ec9024ca254a5c75cc91d2c69bb
johnnywang <johnny_l_wang@manulife.com>
2017-07-27 18:58:44 +0800

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,8 @@ import { SharedModule } from './app.shared.module';
 import { AppComponent } from './app.component';
 import { RequestInterceptor } from './configs/interceptors/request.interceptor';
 import { API } from './configs/api.config';
-// import { TokenProvider } from './services/token.service';
 
 const HttpInterceptorProviders: Type<IHttpInterceptor>[] = [RequestInterceptor];
-
-// export function getAPI(): string {
-//   return API;
-// }
 
 @NgModule({
   declarations: [
@@ -38,8 +33,6 @@ const HttpInterceptorProviders: Type<IHttpInterceptor>[] = [RequestInterceptor];
   ],//本模块声明的组件模板需要的类所在的其它模块
   providers: [
     HttpInterceptorProviders,
-    // TokenProvider
-    // { provide: USERS_API, useFactory: getAPI, }
   ],// 服务的创建者，并加入到全局服务列表中，可用于应用任何部分
   bootstrap: [AppComponent]//指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性
 })
