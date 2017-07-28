1ff94d369b3a81b98d139c0bba52ca044dffeff4
johnnywang <johnny_l_wang@manulife.com>
2017-07-28 15:04:15 +0800

    Initial

--- src/app/core/rest.service.ts
+++ src/app/core/rest.service.ts
@@ -1,26 +1,17 @@
-import { Headers, RequestOptionsArgs, Response, Request, RequestMethod, RequestOptions } from '@angular/http';
-import { HttpInterceptorService } from '@covalent/http';
+import { Headers, RequestOptionsArgs, Response, Request, RequestMethod, RequestOptions, Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { API } from '../core/api.config';
 
 export class RESTService {
-    protected _http: HttpInterceptorService;
 
-    constructor(http: HttpInterceptorService) {
-        this._http = http;
-    }
+    constructor(private http: Http) { }
 
     protected post(url: string, params: any): Observable<any> {
-
-        //  var options = new RequestOptions({ headers: this.buildHeader(url), method: RequestMethod.Post, body: body, search: search })
-
-        // return this._http.request(API.instance.baseUrl + url, options)
-
-        return this._http.post(url, params, { headers: this.buildHeader(url) })
+        return this.http.post(API.instance.baseUrl + url, params, { headers: this.buildHeader(url) })
     }
 
     protected get(url: string): Observable<any> {
-        return this._http.get(API.instance.baseUrl + url, { headers: this.buildHeader(url), method: RequestMethod.Get })
+        return this.http.get(API.instance.baseUrl + url, { headers: this.buildHeader(url), method: RequestMethod.Get })
     }
 
     private buildHeader(url: string): Headers {
@@ -30,15 +21,14 @@ export class RESTService {
         if (url == "/api/token") {
             headers.append('Content-Type', 'application/x-www-form-urlencoded');
             headers.append('Access-Control-Allow-Origin', '*');
-            // headers.append('Authorization', "Basic " + API.instance.clientKey);
-            // headers.append('X-Request-With', null);
+            headers.append('Authorization', "Basic " + API.instance.clientKey);
         } else {
             headers.append('Content-Type', 'application/json');
             headers.append('Access-Control-Allow-Origin', '*');
-            // headers.append('Authorization', "Bearer " + API.instance.get("token"));
+            headers.append('Authorization', "Bearer " + API.instance.get("token"));
         }
 
-        console.log(headers.toJSON())
+        console.log(headers)
 
         return headers;
     }
--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,35 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Response, Headers } from '@angular/http';
-import { HttpInterceptorService } from '@covalent/http';
-import { API } from '../core';
+import { Response, Headers, Http } from '@angular/http';
+import { API, RESTService } from '../core';
 
 @Injectable()
-export class TokenService {
+export class TokenService extends RESTService {
 
-    constructor(private _http: HttpInterceptorService) { }
+    constructor(http: Http) { super(http) }
 
     signIn(username: string, password: string): any {
 
-        var body = new URLSearchParams();
-        body.set("username", username);
-        body.set("password", password);
-        body.set("grant_type", "password");
+        var params = new URLSearchParams();
+        params.set("username", username);
+        params.set("password", password);
+        params.set("grant_type", "password");
 
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        // let headers = new Headers();
+        // headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-        return this._http.post('/api/token', body.toString(), { headers: headers })
-            .map((res: Response) => {
-                var result = res.json();
+        return super.post('/api/token', params.toString()).map((res: Response) => {
+            var result = res.json();
 
-                if (result.status == 1) {
-                    API.instance.set("token", result.data.access_token);
-                    API.instance.set("username", username);
-                }
+            if (result.status == 1) {
+                API.instance.set("token", result.data.access_token);
+                API.instance.set("username", username);
+            }
 
-                return result;
-            });
+            return result;
+        });
     }
+    // signIn(username: string, password: string): any {
+
+    //     var body = new URLSearchParams();
+    //     body.set("username", username);
+    //     body.set("password", password);
+    //     body.set("grant_type", "password");
+
+    //     let headers = new Headers();
+    //     headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    //     return this._http.post('/api/token', body.toString(), { headers: headers })
+    //         .map((res: Response) => {
+    //             var result = res.json();
+
+    //             if (result.status == 1) {
+    //                 API.instance.set("token", result.data.access_token);
+    //                 API.instance.set("username", username);
+    //             }
+
+    //             return result;
+    //         });
+    // }
 }
 
 // export function TokenProviderFactory(
